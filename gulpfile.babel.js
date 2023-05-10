92d187c8ff477a1aeb26d1a1bc3b416449057efb
jonghwa <whdghk3471@gmail.com>
2023-05-11 00:16:26 +0900

    finish_#1.0~#1.1
    finish_#1.0~#1.1

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,21 @@
 import gulp from "gulp";
-import pug from "gulp-pug";
+import gpug from "gulp-pug";
+import del from "del";
 
 const routes = {
   pug: {
     src: "src/*.pug",
+    dest: "build",
   },
 };
 
-export const pug = () => gulp.src("");
+const pug = () =>
+  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-export const dev = () => console.log("I will dev");
+const clean = () => del(["build"]);
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+
+export const dev = gulp.series([prepare, assets]);
