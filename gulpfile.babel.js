f9a89f4447c91972e32d90b4b76d69dae1e93788
Jonghwa <whdghk3471@gmail.com>
2023-05-10 15:17:37 +0900

    until_#1.0_half
    until_#1.0_half

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,12 @@
 import gulp from "gulp";
+import pug from "gulp-pug";
+
+const routes = {
+  pug: {
+    src: "src/*.pug",
+  },
+};
+
+export const pug = () => gulp.src("");
+
+export const dev = () => console.log("I will dev");
