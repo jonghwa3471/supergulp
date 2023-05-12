dfe6b39b4b7788339a31b2542e3cdaee7df470ad
jonghwa <whdghk3471@gmail.com>
2023-05-12 18:05:05 +0900

    all_finish_#2.1
    all_finish_#2.1

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,7 +14,7 @@ const routes = {
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build"]);
+const clean = () => del(["build/"]);
 
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
@@ -27,6 +27,6 @@ const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-const postDev = gulp.series([webserver, watch]);
+const postDev = gulp.parallel([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
